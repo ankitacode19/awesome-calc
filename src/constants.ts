d10b172eebc4bc8551eb269a87045539b4c563df
ankitacode19 <ankitacoder2004@gmail.com>
2024-12-23 18:10:53 +0530

    frontend done 'i guess'

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+const COLORS = [
+    "#000000",  // black
+    "#ffffff",  // white
+    "#9D4811",  // brown
+    "#F2584D",  // red
+    "#D78228",  // orange
+    "#F5E723",  // yellow
+    "#54E355",  // green
+    "#19981A",  // dark green
+    "#1632E9",  // dark blue
+    "#48BFE8",  // baby blue
+    "#8651F2",  // purple
+    "#F09DF3",  // pink
+    
+];
+  
+export { COLORS };
